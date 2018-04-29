84aaa76636f503740983ec59ebbbd476b0dd8a45
Gautham Sunjay <gautham@cs.wisc.edu>
2018-04-29 11:18:25 -0500

    code before piazza

--- xcheck.c
+++ xcheck.c
@@ -9,6 +9,10 @@
 #include "fs.h"
 
 #define PERROR(msg...) fprintf(stderr, msg)
+char arr[8] = { 0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01 };
+// #define BITSET(bitmapblocks, blockaddr) (*(bitmapblocks + blockaddr / 8) & (0x80 >> (blockaddr % 8))) 
+#define BITSET(bitmapblocks, blockaddr) ((*(bitmapblocks + blockaddr / 8)) & (arr[blockaddr % 8])) 
+#define PBITSET(bitmapblocks, blockaddr) printf("blockaddr %u, value %u\n", blockaddr, *(bitmapblocks))
 #define ENABLE_LOGS 0
 
 #define INFO(msg...) if (ENABLE_LOGS) printf(msg)
@@ -22,39 +26,102 @@ typedef struct _image_t {
     char *datablocks;
 } image_t;
 
-int inode_type_test(image_t *image) {
+int check_inode_type(struct dinode *inode) {
+    switch(inode->type) {
+        case T_DIR:
+        case T_FILE:
+        case T_DEV:
+            break;
+        default:
+            return 1;
+    }
+
+    return 0;
+}
+
+int check_inode_direct_blocks(image_t *image, struct dinode *inode) {
+    int i;
+    uint blockaddr;
+    for (i = 0; i < NDIRECT; i++) {
+        blockaddr = inode->addrs[i];
+        if (blockaddr == 0)
+            continue;
+        
+        if (blockaddr < 0 || blockaddr >= image->sb->size || !BITSET(image->bitmapblocks, blockaddr)) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int check_inode_indirect_blocks(image_t *image, struct dinode *inode) {
+    uint blockaddr;
+    blockaddr = inode->addrs[NDIRECT];
+    uint *indirectblk;
+    int i;
+
+    if (blockaddr == 0)
+        return 0;
+
+    if (blockaddr < 0 || blockaddr >= image->sb->size || !(BITSET(image->bitmapblocks, blockaddr))) {
+        return 1;
+    }
+
+    indirectblk = (uint *) (image->datablocks + blockaddr * BSIZE);
+    for (i = 0; i < NINDIRECT; i++, indirectblk++) {
+        blockaddr = *(indirectblk);
+        if (blockaddr == 0)
+            continue;
+        
+        PBITSET(image->bitmapblocks, blockaddr);
+        printf("bit set = %d\n", BITSET(image->bitmapblocks, blockaddr));
+        if (blockaddr < 0 || blockaddr >= image->sb->size || !BITSET(image->bitmapblocks, blockaddr)) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int inode_test(image_t *image) {
     struct dinode *inode;
     int i;
-    char arr[sizeof(struct dinode)];
-    memset(arr, 0, sizeof(struct dinode));
+    int count_not_allocated = 0;
+    //char arr[sizeof(struct dinode)];
 
     inode = (struct dinode *)(image->inodeblocks);
-    int count_not_allocated = 0;
+    //memset(arr, 0, sizeof(struct dinode));
     
     for(i = 0; i < image->sb->ninodes; i++, inode++) {
-        if (memcmp(inode, arr, sizeof(struct dinode)) == 0) {
+        /*if (memcmp(inode, arr, sizeof(struct dinode)) == 0) {
+            count_not_allocated++;
+            continue;
+        }*/
+        if (inode->type == 0) {
             count_not_allocated++;
             continue;
         }
-        
-        switch(inode->type) {
-            case T_DIR:
-            case T_FILE:
-            case T_DEV:
-                break;
-            default:
-                PERROR("ERROR: bad inode.\n");
-                return 1;
+
+        if (check_inode_type(inode) != 0) {
+            PERROR("ERROR: bad inode.\n");
+            return 1;
         }
 
+        if (check_inode_direct_blocks(image, inode) != 0) {
+            PERROR("ERROR: bad direct address in inode.\n");
+            return 1;
+        }
+
+        if (check_inode_indirect_blocks(image, inode) != 0) {
+            PERROR("ERROR: bad indirect address in inode.\n");
+            return 1;
+        }
+        
     }
 
     INFO("Num allocated = %d\n", image->sb->ninodes - count_not_allocated);
     INFO("Num not allocated = %d\n", count_not_allocated);
     return 0;
 }
-
-
 
 
 int fsck(char *filename) {
@@ -79,13 +146,13 @@ int fsck(char *filename) {
 	INFO("nblocks = %u, ninodes = %u, size = %u\n", image.sb->nblocks, image.sb->ninodes, image.sb->size);
 
     image.numinodeblocks = image.sb->ninodes / IPB + 1;
-    image.numbitmapblocks =  (image.sb->nblocks / BSIZE) + 1;
+    image.numbitmapblocks =  (image.sb->nblocks / BPB) + 1;
     
     image.inodeblocks = (char *) (mmapimage + BSIZE * 2);
     image.bitmapblocks = (char *) (image.inodeblocks + BSIZE * image.numinodeblocks);
     image.datablocks = (char *) (image.bitmapblocks + BSIZE * image.numbitmapblocks);
     
-    rv = inode_type_test(&image); 
+    rv = inode_test(&image); 
     if (rv != 0) 
         goto cleanup;
 
